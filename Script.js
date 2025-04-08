2ae0c5f708f78f2a6f6c9b848dd86ddabd0d3cef
IANFOX-King <Ianseloka13@gmail.com>
2025-04-08 11:27:29 +0700

    Script.js

---
+++ Script.js
@@ -0,0 +1,28 @@
+function cekGay() {
+  const name = document.getElementById("name").value.trim();
+  const hasil = document.getElementById("hasil");
+  const gambar = document.getElementById("gambar");
+
+  if (name === "") {
+    hasil.innerText = "Masukkan nama dulu dong~";
+    gambar.style.display = "none";
+    return;
+  }
+
+  const persen = Math.floor(Math.random() * 101); // 0 - 100
+  let komentar = "";
+
+  if (persen < 20) {
+    komentar = "Cowok banget! 😎";
+  } else if (persen < 50) {
+    komentar = "Masih normal kok~";
+  } else if (persen < 80) {
+    komentar = "Hmmm... mencurigakan!";
+  } else {
+    komentar = "Waduh, pelangi detected~!";
+  }
+
+  hasil.innerText = `${name}, kamu ${persen}% gay!, ${komentar}`;
+  gambar.src = "https://media.tenor.com/o3z0euFLoVkAAAAC/chuunibyou-anime.gif";
+  gambar.style.display = "block";
+}
